8a9049074d529a65f7179d9af9db8da70d75764a
yindaheng98 <yindaheng98@gmail.com>
2026-04-25 11:50:19 -0700

    ready for modify

--- feature_4dgs/train.py
+++ feature_4dgs/train.py
@@ -104,3 +104,48 @@ def training(datasets: SequenceFeatureCameraDataset, gaussians_list: List[Semant
         os.makedirs(save_path, exist_ok=True)
         gaussians.save_ply(os.path.join(save_path, "point_cloud.ply"))
         dataset.save_cameras(os.path.join(destination, "cameras.json"))
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--sh_degree", default=3, type=int)
+    parser.add_argument("--name", required=True, type=str)
+    parser.add_argument("--embed_dim", required=True, type=int)
+    parser.add_argument("-s", "--source", required=True, type=str)
+    parser.add_argument("-d", "--destination", required=True, type=str)
+    parser.add_argument("-i", "--iteration", default=30000, type=int)
+    parser.add_argument("-l", "--load_ply", default=None, type=str)
+    parser.add_argument("--load_decoder", default=None, type=str)
+    parser.add_argument("--load_camera", default=None, type=str)
+    parser.add_argument("--no_image_mask", action="store_true")
+    parser.add_argument("--no_depth_data", action="store_true")
+    parser.add_argument("--no_load_semantic", action="store_true")
+    parser.add_argument("--mode", choices=sorted(modes.keys()), default="base")
+    parser.add_argument("--save_iterations", nargs="+", type=int, default=[7000, 30000])
+    parser.add_argument("--device", default="cuda", type=str)
+    parser.add_argument("--dataset_cache_device", default="cpu", type=str)
+    parser.add_argument("--no_preload_dataset_cache", action="store_true")
+    parser.add_argument("--empty_cache_every_step", action='store_true')
+    parser.add_argument("-o", "--option", default=[], action='append', type=str)
+    parser.add_argument("-e", "--option_extractor", default=[], action='append', type=str)
+    args = parser.parse_args()
+    save_cfg_args(args.destination, args.sh_degree, args.source)
+    torch.autograd.set_detect_anomaly(False)
+
+    configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option}
+    extractor_configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option_extractor}
+    dataset, gaussians, trainer = prepare_training(
+        name=args.name, sh_degree=args.sh_degree, mode=args.mode,
+        source=args.source, embed_dim=args.embed_dim,
+        device=args.device, dataset_cache_device=args.dataset_cache_device,
+        trainable_camera="camera" in args.mode,
+        load_ply=args.load_ply, load_decoder=args.load_decoder, load_camera=args.load_camera,
+        load_mask=not args.no_image_mask, load_depth=not args.no_depth_data, load_semantic=not args.no_load_semantic,
+        preload_cache=not args.no_preload_dataset_cache, configs=configs, extractor_configs=extractor_configs)
+    dataset.save_cameras(os.path.join(args.destination, "cameras.json"))
+    torch.cuda.empty_cache()
+    training(
+        dataset=dataset, gaussians=gaussians, trainer=trainer,
+        destination=args.destination, iteration=args.iteration, save_iterations=args.save_iterations,
+        device=args.device, empty_cache_every_step=args.empty_cache_every_step)
